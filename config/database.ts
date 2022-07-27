5525e038ada05872ca82b5dc26f1951c5e1e88bc
Wanderley Ferreira de Albuquerque <wanderleyfa@gmail.com>
2022-07-27 15:54:58 -0300

    feat: adding heroku deploy configs

--- config/database.ts
+++ config/database.ts
@@ -51,6 +51,24 @@ const databaseConfig: DatabaseConfig = {
       healthCheck: false,
       debug: false,
     },
+    pg: {
+      client: 'pg',
+      connection: {
+        host: Env.get('PG_HOST'),
+        port: Env.get('PG_PORT'),
+        user: Env.get('PG_USER'),
+        password: Env.get('PG_PASSWORD', ''),
+        database: Env.get('PG_DB_NAME'),
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+      migrations: {
+        naturalSort: true,
+      },
+      healthCheck: false,
+      debug: false,
+    },
   },
 }
 
